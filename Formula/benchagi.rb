70cb7fab3ca1bc439a2cd44dbe7f0d1ab161e19d
Cory Shelton <coryshelton@Corys-MacBook-Pro-3.local>
2026-06-07 01:48:37 -0600

    benchagi 1.0.0-beta.8: gateway protocol v4 (cloud connect fixed)

--- Formula/benchagi.rb
+++ Formula/benchagi.rb
@@ -20,9 +20,9 @@
 class Benchagi < Formula
   desc "Streaming-aware terminal access to the OpenClaw agent system"
   homepage "https://github.com/BenchAGI/bench-cli"
-  url "https://github.com/BenchAGI/bench-cli/archive/refs/tags/v1.0.0-beta.7.tar.gz"
-  version "1.0.0-beta.7"
-  sha256 "03d9e2594f8a52eeb7685e907d2605c9533492ca99df74a386db8e44a4df8409"
+  url "https://github.com/BenchAGI/bench-cli/archive/refs/tags/v1.0.0-beta.8.tar.gz"
+  version "1.0.0-beta.8"
+  sha256 "e895a0f103cb6536adc403a187423acf39439fb1ed33edcc652dcdcdc6f3ac4c"
   license "MIT"
 
   depends_on "node"
@@ -64,6 +64,6 @@ class Benchagi < Formula
 
   test do
     assert_match "bench v", shell_output("#{bin}/bench version")
-    assert_match "benchagi 1.0.0-beta.7", shell_output("#{bin}/benchagi version")
+    assert_match "benchagi 1.0.0-beta.8", shell_output("#{bin}/benchagi version")
   end
 end
